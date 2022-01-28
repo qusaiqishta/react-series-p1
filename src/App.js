e89a96446f87fbc31b3e34df5aa82c554e38a560
Qusai <qusai@chalktalk.academy>
2022-01-28 15:27:27 +0200

    adding a wrapper and fragments to avoid <div> soup proplem(to many unncessary divs that add no semantic meaning to the DOM )

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React ,{useState}from "react";
+import React ,{useState,Fragment}from "react";
 import AddUser from "./components/Users/AddUser";
 import UserList from "./components/Users/UserList";
 function App() {
@@ -9,10 +9,10 @@ function App() {
  })
   }
   return (
-   <div>
+   <Fragment>
   <AddUser onAddUser={addUserHandler} />
   <UserList users={usersList}/>
-   </div>
+   </Fragment>
   );
 }
 
--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -3,6 +3,7 @@ import classes from "./AddUser.module.css";
 import Card from "../UI/Card";
 import Button from "../UI/Button";
 import ErrorModal from "../UI/ErrorModal";
+import Wrapper from "../Helpers/Wrapper";
 const AddUser = (props) => {
   const [name, setName] = useState("");
   const [age, setAge] = useState("");
@@ -31,7 +32,7 @@ const AddUser = (props) => {
     setError(null);
   };
   return (
-    <div>
+    <Wrapper>
       {error && (
         <ErrorModal
           title={error.title}
@@ -58,7 +59,7 @@ const AddUser = (props) => {
           <Button type="submit">Add User</Button>
         </form>
       </Card>
-    </div>
+    </Wrapper>
   );
 };
 
